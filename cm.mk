7f26b8ab484e84e13b4987b21c08c4c2f31a253d
Fábio Silva <fabio.fernando.osilva@gmail.com>
2012-10-07 11:01:51 -0300

    Fixed specific device, product, etc. for each device. (2/3)
    Change-Id: I33df140d608d48e0c6b20ed92765e4aa60c712dd

--- cm.mk
+++ cm.mk
@@ -13,7 +13,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 # --------------------------------------------------------------------------------
 
-$(call inherit-product, device/samsung/p1/p1.mk)
+$(call inherit-product, device/samsung/p1n/full_p1n.mk)
 
 # --------------------------------------------------------------------------------
 # product specific settings
@@ -29,3 +29,10 @@ TARGET_KERNEL_CONFIG := cyanogenmod_$(PRODUCT_DEVICE)_defconfig
 
 # asserts
 TARGET_OTA_ASSERT_DEVICE := galaxytab,$(PRODUCT_MODEL)
+
+# Set build fingerprint / ID / product name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+       PRODUCT_NAME=GT-P1000N \
+       TARGET_DEVICE=GT-P1000N \
+       BUILD_FINGERPRINT=samsung/GT-P1000N/GT-P1000N:2.3.3/GINGERBREAD/UBJP7:user/release-keys \
+       PRIVATE_BUILD_DESC="GT-P1000N-user 2.3.3 GINGERBREAD UBJP7 release-keys"
